d63886f0d2574adb13324dd3fb4c1adb168430b0
Dosunmu Olawole <103733962+Lawole@users.noreply.github.com>
2023-08-14 12:11:16 +0100

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -12,15 +12,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Eazy Level - Order not randomised:
-# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-# Hard Level - Order of characters randomised:
-# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-
-
 password_list = []
-# the password has to be a list in order for the shuffle() function to work. hence we are creating an empty list to
-# contain the not generated password characters
 
 for letter in range(0, nr_letters):
     password_list += random.choice(letters)
@@ -37,14 +29,7 @@ random.shuffle(password_list)
 # print(password_list)
 
 password = ""
-# Now we have to create an empty password string to capture the password characters, for we to be able to turn the
-# earlier created password_list to a string. And by doing this we need a for loop to loop through the password_list
-# list to add each character to the empty password string now created
+
 for lett in password_list:
     password += lett
 print(f" Your password is: {password}")
-
-# An example to show me how to use the range function on a list
-# lst = [10, 50, 75, 83, 98, 84, 32]
-# for x in range(len(lst)):
-#     print(lst[x])
